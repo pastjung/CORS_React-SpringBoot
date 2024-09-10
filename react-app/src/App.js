864c795c1664e5bc385b0d28b26f96939453b823
정재현 <chwogus0303@naver.com>
2024-09-10 19:20:12 +0900

    feat(CORS): React + Springboot CORS 연결 확인 코드 추가

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const springbootApiPort = process.env.REACT_APP_SPRINGBOOT_HOST_PORT;
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // React 실행시 Spring Boot 백엔드 API를 호출
+    axios.get(`http://localhost:${springbootApiPort}/api/hello`)
+      .then(response => setMessage(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
